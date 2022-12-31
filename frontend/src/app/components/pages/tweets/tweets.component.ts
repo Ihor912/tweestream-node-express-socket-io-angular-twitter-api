e599ff2dd57b8c4589ee0ce83e11427e1d2ad77d
ihor-khomiak <iokhomiak@gmail.com>
2022-12-30 21:24:11 -0800

    feat: [frontend] use base-page-component for pages

--- frontend/src/app/components/pages/tweets/tweets.component.ts
+++ frontend/src/app/components/pages/tweets/tweets.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, takeUntil } from 'rxjs';
 import { DataStreamFacade, DataStreamingRuleFacade } from '../../../store';
 import { ErrorHandlingService } from '../../../services';
 import {
@@ -46,65 +46,40 @@ export class TweetsComponent extends BasePageComponent {
     //   this.dataStreamingRuleFacade.setRules({ rules: [{ value: 'ukraine' }] });
     // }, 15000);
 
-    this.dataStreamingRuleFacade.setRulesSuccess$.subscribe(
-      (resp: RulesStatusResponse | null) => {
-        if (resp) {
-          console.log(resp);
-        }
-      }
-    );
-    this.dataStreamingRuleFacade.setRulesError$.subscribe(
-      (error: HttpErrorResponse | null | undefined) => {
+    this.dataStreamingRuleFacade.setRulesError$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((error: HttpErrorResponse | null | undefined) => {
         if (error) {
           this.errorHandlingService.handleHttpError(error);
         }
-      }
-    );
+      });
 
-    this.dataStreamingRuleFacade.deleteRulesSuccess$.subscribe(
-      (resp: RulesStatusResponse | null) => {
-        if (resp) {
-          console.log(resp);
-        }
-      }
-    );
-    this.dataStreamingRuleFacade.deleteRulesError$.subscribe(
-      (error: HttpErrorResponse | null | undefined) => {
+    this.dataStreamingRuleFacade.deleteRulesError$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((error: HttpErrorResponse | null | undefined) => {
         if (error) {
           this.errorHandlingService.handleHttpError(error);
         }
-      }
-    );
+      });
 
-    this.dataStreamFacade.getDataStreamSuccess$.subscribe(
-      (tweet: Tweet | null) => {
+    this.dataStreamFacade.getDataStreamSuccess$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((tweet: Tweet | null) => {
         if (tweet) {
           this.tweetStream.unshift(tweet);
         }
-      }
-    );
-    this.dataStreamFacade.getDataStreamError$.subscribe(
-      (error: StreamConnectionError | null | undefined) => {
+      });
+    this.dataStreamFacade.getDataStreamError$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((error: StreamConnectionError | null | undefined) => {
         if (error) {
           this.errorHandlingService.handleStreamConnectionError(error);
         }
-      }
-    );
+      });
+  }
 
-    this.dataStreamFacade.stopDataStreamSuccess$.subscribe(
-      (resp: StreamStatusResponse | null | undefined) => {
-        if (resp) {
-          console.log(resp);
-        }
-      }
-    );
-    this.dataStreamFacade.stopDataStreamError$.subscribe(
-      (error: StreamConnectionError | null | undefined) => {
-        if (error) {
-          this.errorHandlingService.handleStreamConnectionError(error);
-        }
-      }
-    );
+  tweetTrackBy(index: number, tweet: Tweet) {
+    return tweet.id;
   }
 
   override onDestroy() {
